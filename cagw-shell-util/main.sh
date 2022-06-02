cf0f36df8d610859ac1c583070b1f4a84fb5beb5
anuragj-ent <anurag.jain@entrust.com>
2022-06-02 15:26:26 -0400

    Generate CSR

--- cagw-shell-util/main.sh
+++ cagw-shell-util/main.sh
@@ -11,18 +11,35 @@ get_client_credentials() {
 }
 main() {
 	echo "Select the CA Gateway operation:
-1. List all Certificate Authorities
-2. List all profiles for a Certificate Authority
-3. Enroll new certificate
-4. Exit"
+1. Generate CSR with subject (using OpenSSL)
+2. List all Certificate Authorities
+3. List all profiles for a Certificate Authority
+4. Enroll new certificate with CSR
+5. Exit"
 	read -r CAGW_OP
-	#echo $P12$CAGW_URL$CAGW_OP
 	if [ $CAGW_OP == "1" ]
 	then
-		echo "Getting all CAs"
-		curl  --header "Accept: application/json" -H "Content-Type: application/json" --cert-type P12 --cert $P12:$P12_PWD $CAGW_URL/v1/certificate-authorities
+		echo "Enter full subject
+Example: /C=CA/ST=Ontario/L=Ottawa/O=My Org/OU=IT/CN=example.com"
+		read -r CSR_SUBJECT
+		echo "Enter key type: "
+		read -r KEY_TYPE
+		echo "Enter key length: "
+		read -r KEY_LEN
+		echo "Where would you like to store the key (e.g. /tmp/example.key): "
+		read -r KEY_PATH
+		echo "Where would you like to store the CSR (e.g. /tmp/example.csr): "
+		read -r CSR_PATH
+		openssl req -nodes -newkey $KEY_TYPE:$KEY_LEN -keyout $KEY_PATH -out $CSR_PATH -subj \"$CSR_SUBJECT\"
+		wait
+		echo "-----Generated CSR-----"
+		cat $CSR_PATH
 		main
 	elif [ $CAGW_OP == "2" ]
+	then
+		curl  --header "Accept: application/json" -H "Content-Type: application/json" --cert-type P12 --cert $P12:$P12_PWD $CAGW_URL/v1/certificate-authorities
+		main
+	elif [ $CAGW_OP == "3" ]
 	then
 		echo "Enter CA ID: "
 		read -r CAID
@@ -30,9 +47,17 @@ main() {
 		main
 	elif [ $CAGW_OP == "4" ]
 	then
+		echo "Enter CA ID: "
+		read -r CAID
+		echo "Enter certificate profile ID: "
+		read -r PROFILE_ID
+		curl  --header "Accept: application/json" -H "Content-Type: application/json" --cert-type P12 --cert $P12:$P12_PWD $CAGW_URL/v1/certificate-authorities/$CAID/profiles
+		main
+	elif [ $CAGW_OP == "5" ]
+	then
 		exit
 	else
-		echo "something else"
+		echo "Invalid Selection"
 	fi
 }
 echo "--------------------------
